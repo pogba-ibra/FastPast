5e54b3d71c19329dfc914591a00ea43a569c3bef
ZLATAN <zlatan@fastpast.com>
2025-12-18 19:06:52 +0100

    Fix: Remove unnecessary escape characters in server.js

--- server.js
+++ server.js
@@ -3028,7 +3028,7 @@ async function fetchPageMetadata(url, requestUA) {
         'User-Agent': requestUA || "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
         'Accept-Language': 'en-US,en;q=0.9',
-        'Sec-Ch-Ua': '"Not A(Brand";v="99", "Google Chrome";v="143\", \"Chromium\";v=\"143\"',
+        'Sec-Ch-Ua': '"Not A(Brand";v="99", "Google Chrome";v="143", "Chromium";v="143"',
         'Sec-Ch-Ua-Mobile': '?0',
         'Sec-Ch-Ua-Platform': '"Windows"',
         'Upgrade-Insecure-Requests': '1',
